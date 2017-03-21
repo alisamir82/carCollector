fac332ef1c8c1c0df5a29e9d31e5d6beb2bd4334
Ali <ali@iMac.local>
2017-03-21 20:50:08 +0000

    Imagepicker code is added

--- CarViewController.swift
+++ CarViewController.swift
@@ -8,22 +8,40 @@
 
 import UIKit
 
-class CarViewController: UIViewController {
-
+class CarViewController: UIViewController , UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
     @IBOutlet weak var carImageView: UIImageView!
     @IBOutlet weak var titleTextField: UITextField!
+    
+    var imagePicker = UIImagePickerController()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        
     }
-
+    
+    
+    
     @IBAction func cameraTapped(_ sender: Any) {
     }
-
+    
     @IBAction func photosTapped(_ sender: Any) {
+        
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
     }
-
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        carImageView.image = image
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
     @IBAction func addTapped(_ sender: Any) {
     }
 }
