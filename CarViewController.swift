a269a3accb122bc594e4e586e24514456cb32064
Ali <ali@iMac.local>
2017-03-21 13:39:54 +0000

    carViewController is set and ready for some code.

---
+++ CarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CarViewController.swift
+//  CarCollector
+//
+//  Created by Work on 21/03/2017.
+//  Copyright © 2017 compume. All rights reserved.
+//
+
+import UIKit
+
+class CarViewController: UIViewController {
+
+    @IBOutlet weak var carImageView: UIImageView!
+    @IBOutlet weak var titleTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+}
