f8e29240c951cb68b2870fccbe605819dd75ec81
Ali <ali@iMac.local>
2017-03-23 22:21:25 +0000

    no message

--- CarCollector/ViewController.swift
+++ CarCollector/ViewController.swift
@@ -8,18 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var cars : [Car] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+       
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cars.count
     }
-
-
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let car = cars[indexPath.row]
+        performSegue(withIdentifier: "addUpdateSegue", sender: car)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nextVC = segue.destination as! CarViewController
+        nextVC.car = sender as? Car
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let car = cars[indexPath.row]
+        cell.textLabel?.text = car.title
+        cell.imageView?.contentMode = .scaleAspectFill
+        cell.imageView?.image = UIImage(data: car.image as! Data)
+        
+        return cell
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        do{
+            cars = try context.fetch(Car.fetchRequest())
+            tableView.reloadData()
+        } catch{
+            
+        }
+        
+    }
+    
 }
+
 
--- CarViewController.swift
+++ CarViewController.swift
@@ -13,19 +13,35 @@ class CarViewController: UIViewController , UIImagePickerControllerDelegate, UIN
     @IBOutlet weak var carImageView: UIImageView!
     @IBOutlet weak var titleTextField: UITextField!
     
+    @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet weak var addUpdateButton: UIButton!
+    
     var imagePicker = UIImagePickerController()
     
+    var car : Car? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         imagePicker.delegate = self
         
+        if car != nil {
+            carImageView.image = UIImage(data: car?.image as! Data)
+            titleTextField.text = car?.title
+            addUpdateButton.setTitle("Update", for: .normal)
+        }else
+        {
+            deleteButton.isHidden = true
+            
+        }
+        
     }
     
     
     
     @IBAction func cameraTapped(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
     }
     
     @IBAction func photosTapped(_ sender: Any) {
@@ -42,6 +58,37 @@ class CarViewController: UIViewController , UIImagePickerControllerDelegate, UIN
         
         imagePicker.dismiss(animated: true, completion: nil)
     }
+    
     @IBAction func addTapped(_ sender: Any) {
+        
+        
+        if car != nil {
+            
+            car!.title = titleTextField.text!
+            car!.image = UIImagePNGRepresentation(carImageView.image!) as NSData?
+            
+            
+        }else {
+            
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            let car = Car(context: context)
+            
+            car.title = titleTextField.text!
+            car.image = UIImagePNGRepresentation(carImageView.image!) as NSData?
+            
+        }
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController!.popViewController(animated: true)
+    }
+    
+    
+    @IBAction func deleteButton(_ sender: Any) {
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        context.delete(car!)
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+         navigationController!.popViewController(animated: true)
     }
 }
